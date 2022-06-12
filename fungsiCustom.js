172a413c2f8605265f43e311af5249d332b446d8
Sulkhi <sulkhi25@gmail.co>
2022-06-12 20:53:06 +0700

    bikin multi read file

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,8 +19,32 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
-
+const bacaData = (fn) => {
+  var promises = [file1, file2, file3].map(function (f) {
+    return new Promise(
+      function (_path, resolve, reject) {
+        fs.readFile(_path, "utf8", function (err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            // parsing data dirty way, sorry JSON.parse 😂
+            resolve(
+              data
+                .match(/message": ?"(.*)"/)[1]
+                .replace(/halo/gi, "")
+                .trim()
+            );
+          }
+        });
+      }.bind(this, f)
+    );
+  });
+  return Promise.all(promises)
+    .then((r) => {
+      fn(null, r);
+    })
+    .catch((er) => fn(er, null));
+};
 // ! JANGAN DIMODIFIKASI
 module.exports = {
   modifyFile1,
